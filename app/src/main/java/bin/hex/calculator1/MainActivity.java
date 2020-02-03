501971fdfea70a7fe75bd35746c8e09a7c0ac5b4
aleksandr <pilyanzo@gmail.com>
2020-02-03 12:36:08 +0100

    calculatorbinhex

---
+++ app/src/main/java/bin/hex/calculator1/MainActivity.java
@@ -0,0 +1,14 @@
+package bin.hex.calculator1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
