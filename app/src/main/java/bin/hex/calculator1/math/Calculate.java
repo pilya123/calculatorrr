a815083b5021a121f0b9be9d64d226d7100daf64
aleksandr <pilyanzo@gmail.com>
2020-02-09 11:25:57 +0100

    calculatorbinhex

--- app/src/main/java/bin/hex/calculator1/math/Calculate.java
+++ app/src/main/java/bin/hex/calculator1/math/Calculate.java
@@ -64,5 +64,60 @@ public class Calculate {
     }
 
 
+    // Java program to illustrate
+// bitwise operators
+   /* public class operators {
+        public static void main(String[] args)
+        {
+            //Initial values
+            int a = 5;
+            int b = 7;
+
+            // bitwise AND
+            // 0101 & 0111=0101 = 5
+            System.out.println("a&b = " + (a & b));
+
+            // bitwise OR
+            // 0101 | 0111=0111 = 7
+            System.out.println("a|b = " + (a | b));
+
+            // bitwise XOR
+            // 0101 ^ 0111=0010 = 2
+            System.out.println("a^b = " + (a ^ b));
+
+            // bitwise AND
+            // ~0101=1010
+            // will give 2's complement of 1010 = -6
+            System.out.println("~a = " + ~a);
+
+            // can also be combined with
+            // assignment operator to provide shorthand
+            // assignment
+            // a=a&b
+            a &= b;
+            System.out.println("a= " + a);
+        }
+    }
+    // Java program to illustrate
+// SHIFT operators
+public class operators {
+    public static void main(String[] args)
+    {
+
+        int a = 5;
+        int b = -10;
+
+        // LEFT shift operator
+        // 0000 0101<<2 =0001 0100(20)
+        // similar to 5*(2^2)
+        System.out.println("a<<2 = " + (a << 2));
+
+        // RIGHT shift operator
+        // 0000 0101 >> 2 =0000 0001(1)
+        // similar to 5/(2^2)
+        System.out.println("b>>2 = " + (b >> 2));
+    }
+}
+    */
 
 }
