7cffbe6f4a61857b336b2eff42814e2e82ceda42
maxim <laba1203@gmail.com>
2020-10-03 14:03:27 +0300

    parity code UI/UX/Formula

--- app/src/main/java/bin/hex/calculator1/pages/HammingParityPage.java
+++ app/src/main/java/bin/hex/calculator1/pages/HammingParityPage.java
@@ -8,14 +8,16 @@ import android.widget.RadioGroup;
 
 import bin.hex.calculator1.R;
 import bin.hex.calculator1.elements.InputElement;
-import bin.hex.calculator1.elements.OperatorsRadioGroup;
 import bin.hex.calculator1.elements.RadioGroupElement;
 import bin.hex.calculator1.math.Calculate;
+import bin.hex.calculator1.math.ParityOfNumber;
 import bin.hex.calculator1.util.Log;
 
 public class HammingParityPage extends AbstractPage {
 
-    private InputElement hammingInput, hammingResult, endiannessInput, endiannessResult;
+    private InputElement hammingInput, hammingResult,
+            endiannessInput, endiannessResult,
+            parityInput, parityBruteForce, parityBasedOnClearingSetBit, parityMostEfficient, parityWithCaching;
     private String convertedHammingValue, convertedEndiannessValue = "";
     private RadioGroupElement endiannessType;
 
@@ -31,6 +33,13 @@ public class HammingParityPage extends AbstractPage {
         endiannessInput = new InputElement(findViewById(R.id.input_endianness_value));
         endiannessResult = new InputElement(findViewById(R.id.output_endianness));
         endiannessType = new RadioGroupElement((RadioGroup) findViewById(R.id.endian_type));
+
+        parityInput = new InputElement(findViewById(R.id.parity_input));
+        parityBruteForce = new InputElement(findViewById(R.id.parity_bruce_force));
+        parityBasedOnClearingSetBit = new InputElement(findViewById(R.id.parity_based_on_clearing_set_bin));
+        parityMostEfficient = new InputElement(findViewById(R.id.parity_most_efficient));
+        parityWithCaching = new InputElement(findViewById(R.id.parity_with_cashing));
+
     }
 
     public void onConvertHammingClick(View view) {
@@ -60,6 +69,35 @@ public class HammingParityPage extends AbstractPage {
         convertedEndiannessValue = Calculate.convertEndianness(initial, isLittle);
 
         updateValueOnUi(endiannessResult, convertedEndiannessValue);
+
+    }
+
+    public void onConvertParityClick(View view) {
+        String initial = parityInput.getConvertedValue();
+
+        if (initial == null || initial.isEmpty()) {
+            showError("Please type initial value.");
+            return;
+        }
+        long convertedInitial;
+        try {
+            convertedInitial = Long.parseLong(initial);
+        }catch (Exception e){
+            e.printStackTrace();
+            showError("Incorrect Initial Parity value: " + initial);
+            return;
+        }
+        Log.log("[Parity] Binary representation of the number: " + Long.toBinaryString(convertedInitial));
+
+        String computeParityBruteForce = String.valueOf(ParityOfNumber.computeParityBruteForce(convertedInitial));
+        String computeParityBasedOnClearingSetBit = String.valueOf(ParityOfNumber.computeParityBasedOnClearingSetBit(convertedInitial));
+        String computeParityMostEfficient = String.valueOf(ParityOfNumber.computeParityMostEfficient(convertedInitial));
+        String computeParityWithCaching = String.valueOf(ParityOfNumber.computeParityWithCaching(convertedInitial));
+
+        updateValueOnUi(parityBruteForce, computeParityBruteForce);
+        updateValueOnUi(parityBasedOnClearingSetBit, computeParityBasedOnClearingSetBit);
+        updateValueOnUi(parityMostEfficient, computeParityMostEfficient);
+        updateValueOnUi(parityWithCaching, computeParityWithCaching);
 
     }
 
